3e965d8244d689a688df8f1b941c0d15e0005539
Ronald Kim <75988771+arete89@users.noreply.github.com>
2021-07-14 22:30:08 +0900

    Update FA_rating.py

--- FA_rating.py
+++ FA_rating.py
@@ -7,6 +7,8 @@ api_key = ""
 ticker = "TSLA"
 
 rating_df = fa.rating(ticker, api_key)
+print(rating_df)
+#slice a specific recommendation from the dataframe
 rating_df['ratingDetailsPBRecommendation']
 
 #Export the gathered data to an Excel spreadsheet
